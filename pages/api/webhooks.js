e0c59a09d9bb7cbafeef9f0cc021e9aa304a9856
k3nt0w <kento.watanabe.11@gmail.com>
2023-10-17 21:16:13 +0900

    fix

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -1,11 +1,16 @@
+// In-memory storage to hold the data
+let dataStore = [];
+
 export default async (req, res) => {
-  const query = req.query;
   if (req.method === 'POST') {
+    // Store the POST request body data to dataStore array
+    dataStore.push(req.body);
     console.log(req.body)
     res.status(200).send('OK');
   } else if (req.method === 'GET') {
-    res.status(200).send(query['hub.challenge']);
-  } else  {
+    // On GET request, respond with the dataStore contents
+    res.status(200).json(dataStore);
+  } else {
     res.status(405).send('Method Not Allowed');
   }
 };
