1f6b24dea5a9767f34981ddb06349e85ab5e5a9a
k3nt0w <kento.watanabe.11@gmail.com>
2023-10-17 20:48:03 +0900

    fix

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,8 +1,8 @@
 export default async (req, res) => {
   if (req.method === 'POST') {
-    res.status(200).send('OK');
+    res.status(200).send('1071378917');
   } else if (req.method === 'GET') {
-    res.status(200).send('OK');
+    res.status(200).send('1071378917');
   } else  {
     res.status(405).send('Method Not Allowed');
   }
