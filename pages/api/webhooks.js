79fd73d28064363609691303af80bcf15495c9f8
k3nt0w <kento.watanabe.11@gmail.com>
2023-10-17 21:03:52 +0900

    log

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -1,6 +1,7 @@
 export default async (req, res) => {
   const query = req.query;
   if (req.method === 'POST') {
+    console.log(req.body)
     res.status(200).send('OK');
   } else if (req.method === 'GET') {
     res.status(200).send(query['hub.challenge']);
