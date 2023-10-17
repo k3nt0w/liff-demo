c7be71c55d75272cdfef4f37826e34b33a779d82
k3nt0w <kento.watanabe.11@gmail.com>
2023-10-17 20:54:46 +0900

    fix

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -1,8 +1,10 @@
 export default async (req, res) => {
+  const { hub } = req.query;
+
   if (req.method === 'POST') {
-    res.status(200).send('1071378917');
+    res.status(200).send('OK');
   } else if (req.method === 'GET') {
-    res.status(200).send('1071378917');
+    res.status(200).send(hub.challenge);
   } else  {
     res.status(405).send('Method Not Allowed');
   }
