364c21da5c935d295d690efda5760093d728f519
k3nt0w <kento.watanabe.11@gmail.com>
2023-10-23 20:30:16 +0900

    feat: hub.challenge

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -8,6 +8,10 @@ export default async (req, res) => {
     console.log(req.body)
     res.status(200).send('OK');
   } else if (req.method === 'GET') {
+    const { 'hub.challenge': hubChallenge } = req.query
+    if (hubChallenge) {
+      res.status(200).send(hubChallenge);
+    }
     // On GET request, respond with the dataStore contents
     res.status(200).json(dataStore);
   } else {
